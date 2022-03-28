cccc9f2679320a95c6c58e7fac1d495c5efc6a80
dedisalam <dedispermana20@gmail.com>
2022-03-28 20:38:33 +0700

    global type problem

--- src/variables/global/types.ts
+++ src/variables/global/types.ts
@@ -6,12 +6,38 @@ type Obj = {
 
 type BackgroundColor = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark' | 'black' | 'white' | 'body' | 'transparent';
 
+type Spacers = '0' | '1' | '2' | '3' | '4' | '5';
+// type MARGIN_BASE =
+//   'all' |
+//   'x' |
+//   'y' |
+//   'top' |
+//   'end' |
+//   'bottom' |
+//   'start';
+// function margins() {
+//   const Obj: Obj = {};
+//   Object.keys(MARGIN_BASE).forEach((BASE) => {
+//     Obj[`margin-${BASE}`] = MARGIN_BASE[BASE];
+//   });
+//   return Obj;
+// }
+// console.log(margins());
+
 type GlobalVar = {
   'background-color': Obj;
 };
 
+// console.log(margins());
+// const test = [
+//   'margin-end',
+//   'margin-start',
+// ];
+// type Mykey = 'margin-end' | 'margin-start';
 type GlobalProps = {
   'background-color'?: BackgroundColor;
+  'margin-end'?: Spacers | 'auto';
+  'margin-top'?: Spacers | 'auto';
   id?: string;
 };
 
